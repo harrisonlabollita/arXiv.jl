f134a0c5e5d2a92a7af2427eb6e6cc11c24a99f2
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-07-14 11:00:00 -0700

    some basics

--- src/arXiv.jl
+++ src/arXiv.jl
@@ -1,4 +1,42 @@
 module arXiv
+using HTTP
+using LightXML
 
+
+function extractBibInfo(Entries::Array)
+	bibs = []
+	for entry in Entries
+	    bibDict = Dict()
+	    url = strip(content(find_element(entry, "id")))
+	    bibDict["url"]     =  url 
+	    bibDict["authors"] =  [strip(content(el)) for el in find_all_elements(entry, "author")]
+	    bibDict["journal"] =  "arXiv:$url[22:end]"  # TO-DO: this is rigid might not always work
+	    push!(bibs, bibDict)
+	end
+	return bibs
+end
+
+	
+
+function find_all_elements(x::XMLElement, n::AbstractString)
+	matched = []
+	for c in child_elements(x)
+		name(c) == n && push!(matched, c)
+	end
+	return matched
+end
+
+
+
+function request(search::String; field="all", sortBy=nothing, sortOrder=nothing)
+	base = "http://export.arxiv.org/api/query?"
+	r    = HTTP.request(:GET, base)
+	extractBibInfo(parse_string(String(r.body)))
+	
+end
+
+function bibtex()
+
+end
 
 end # module
